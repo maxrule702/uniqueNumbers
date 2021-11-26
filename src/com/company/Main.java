21bf12c03921e31df32244a7ec34a2426473861b
Max Churchill <MC211708@reigate.ac.uk>
2021-11-26 09:43:54 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String[] numbersName = new String [5];
+        System.out.println("please chose a number form 1,5");
+        System.out.println("please enter your name");
+        String userName = input.next();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("please guess a number");
+            int userinput = input.nextInt();
+            if (!userName.equals(numbersName)|| ){
+
+
+
+
+            }
+
+
+
+
+        }
+    }
+}
